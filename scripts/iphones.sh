f62f178c3b27cba31dbc222372174a06372c5deb
Maurizio Branca <maurizio.branca@gmail.com>
2023-02-19 08:37:56 +0100

    Add iPhone automation

---
+++ scripts/iphones.sh
@@ -0,0 +1,9 @@
+
+. venv/bin/activate
+
+rfrb it iphones --name "13 Pro Max" --format json > iphones.json
+
+cat iphones.json | \
+    jq '{"products": .}' | \
+    jinja -d - -f json templates/refurbished/iphones.j2 | \
+    tgm message send --chat-id ${CHAT_ID}
