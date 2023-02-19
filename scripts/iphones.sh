790d7fe9c5f106fb8a0deb35e447133e12ade19b
Maurizio Branca <maurizio.branca@gmail.com>
2023-02-19 09:03:01 +0100

    Change dir before run automation

--- scripts/iphones.sh
+++ scripts/iphones.sh
@@ -1,4 +1,6 @@
 
+pushd code/projects/zmoog/automator
+
 . venv/bin/activate
 
 rfrb it iphones --name "13 Pro Max" --format json > iphones.json
@@ -7,3 +9,5 @@ cat iphones.json | \
     jq '{"products": .}' | \
     jinja -d - -f json templates/refurbished/iphones.j2 | \
     tgm message send --chat-id ${CHAT_ID}
+
+popd
