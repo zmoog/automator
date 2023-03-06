12b63d77de92027b047b98e8fc41813109e6248e
Maurizio Branca <maurizio.branca@gmail.com>
2023-03-06 06:31:22 +0100

    Fix PATH handling

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ set -e
 [ -z ${AUTOMATOR_HOME} ] && echo "AUTOMATOR_HOME is not set" && exit 1
 
 # Add non-Python tools to the PATH.
-export PATH=$PATH/tools
+export PATH=$PATH:$AUTOMATOR_HOME/tools
 
 [ $# -eq 0 ] && echo "No arguments provided" && exit 1
 
